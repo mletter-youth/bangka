8d0cb9743d38338022b6190baaae44ba668970c6
minji01234 <minji0123@toobplus.com>
2025-12-04 17:24:28 +0900

    화면 가운데 메시지 표시추가

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let slideStart = document.querySelector('.slide-start');
+let slideEnd = document.querySelector('.slide-end');
+
 const slide = new Swiper('.swiper',{
     direction: 'horizontal',
     autoplay: false,
@@ -17,3 +20,25 @@ const slide = new Swiper('.swiper',{
         },
     }
 })
+slide.on('slideChange', function(){
+    if(this.isEnd){
+        slideEnd.classList.add('active');
+        setTimeout(() => {
+           slideEnd.classList.remove('active');
+        }, 3000);
+    }
+    else{
+        slideEnd.classList.remove('active');
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    slideStart.classList.add('active');
+    setTimeout(() => {
+        slideStart.classList.remove('active');
+    }, 3000);
+
+    
+})
+
+let nextBtn = document.querySelector('.swiper-button-next');
